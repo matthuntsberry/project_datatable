7735ea942055c61a8fee39de444e5aa209a64be0
matthuntsberry <matthew.marshall.huntsberry@ibm.com>
2019-11-08 14:29:49 -0600

    removed apollo related items

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
-import ApolloClient from "apollo-boost";
-import { ApolloProvider } from "react-apollo";
 
 // IE11 Polyfills
 import "core-js/modules/es7.array.includes";
@@ -14,18 +12,9 @@ import "core-js/modules/es7.object.values";
 import "./scss/vendors/carbon.scss";
 import App from "./components/App";
 
-const client = new ApolloClient({
-  uri: "https://api.github.com/graphql",
-  headers: {
-    authorization: `Bearer ${process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN}`
-  }
-});
-
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <Router>
-      <App />
-    </Router>
-  </ApolloProvider>,
+  <Router>
+    <App />
+  </Router>,
   document.getElementById("root")
 );
